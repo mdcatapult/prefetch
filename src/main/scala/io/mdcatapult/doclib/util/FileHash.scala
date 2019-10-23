2f83d96acedc5a716eabd3d420874514cc08a4d7
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2019-10-23 11:46:40 +0100

    If filename is too long then use md5 hash instead

--- src/main/scala/io/mdcatapult/doclib/util/FileHash.scala
+++ src/main/scala/io/mdcatapult/doclib/util/FileHash.scala
@@ -2,6 +2,10 @@ package io.mdcatapult.doclib.util
 
 import java.io.{File, FileInputStream}
 import java.security.{DigestInputStream, MessageDigest}
+import java.math.BigInteger
+
+import io.lemonlabs.uri.Uri
+import better.files.{File ⇒ ScalaFile, _}
 
 trait FileHash {
   /**
@@ -19,5 +23,55 @@ trait FileHash {
       dis.close()
     }
     md5.digest.map("%02x".format(_)).mkString
+  }
+
+  def md5HashString(s: String): String = {
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(s.getBytes)
+    val bigInt = new BigInteger(1,digest)
+    val hashedString = bigInt.toString(16)
+    hashedString
+  }
+
+  /**
+   * Check the original file name against the new (possibly query hashed) one and if too long
+   * then hash the orig name so we end up with name like 134636346.2463547536.pdf
+   * @param origUri
+   * @param fileName
+   * @return
+   */
+  def hashOrOriginal(origUri: Uri, fileName: String): String = {
+    fileName.length match {
+      case length if length >= 256 ⇒ {
+        val origFile = ScalaFile(origUri.path.toString())
+        val origFileName = origFile.nameWithoutExtension
+        val origExtension = origFile.extension.getOrElse("").replaceFirst(".", "")
+        val queryHash = fileName.replace(origFileName, "").replace(origExtension, "").replace(".", "")
+        s"${newFileName(origFileName, queryHash, origExtension)}"
+      }
+      case _ ⇒ fileName
+    }
+  }
+
+  /**
+   * Given a filename, hash of the url query params and a file extension it
+   * returns new file name in form fileNameHash.queryHash.fileExtension
+   * @param fileName
+   * @param queryHash
+   * @param fileExtension
+   * @return
+   */
+  def newFileName(fileName: String, queryHash: String, fileExtension: String): String = {
+    s"${md5HashString(fileName)}${
+      queryHash match {
+        case "" ⇒ ""
+        case (value) ⇒ s".$value"
+      }
+    }${
+      fileExtension match {
+        case "" ⇒ ""
+        case (value) ⇒ s".$value"
+      }
+    }"
   }
 }
--- src/test/scala/io/mdcatapult/doclib/remote/adapters/HttpSpec.scala
+++ src/test/scala/io/mdcatapult/doclib/remote/adapters/HttpSpec.scala
@@ -57,5 +57,65 @@ class HttpSpec extends FlatSpec {
     }
     assert(caught.getMessage == "Unable to process URL with resolved status code of 404")
   }
-  
+
+  "A filename that is shorter than 255 chars" should "not be changed" in {
+    val origUri = Uri.parse("http://eprints.hud.ac.uk/10819/1/MorralNew.pdf")
+    val fileName = "MorralNew.pdf"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
+  "A uri with calculated filename that is longer than 255 chars with query params" should "be hashed" in {
+    val origUri = Uri.parse("http://orbit.dtu.dk/en/publications/dual-nicotinic-acetylcholine-receptor-42-antagonists7-agonists-synthesis-docking-studies-and-pharmacological-evaluation-of-tetrahydroisoquinolines-and-tetrahydroisoquinolinium-salts(040536e1-22a7-47c7-bee9-51e53543ff04).pdf?nofollow=true&rendering=author")
+    val fileName = "dual-nicotinic-acetylcholine-receptor-42-antagonists7-agonists-synthesis-docking-studies-and-pharmacological-evaluation-of-tetrahydroisoquinolines-and-tetrahydroisoquinolinium-salts(040536e1-22a7-47c7-bee9-51e53543ff04).74bdbe0d010151d2d42f6768eca1290c.pdf"
+    val actualName = "dual-nicotinic-acetylcholine-receptor-42-antagonists7-agonists-synthesis-docking-studies-and-pharmacological-evaluation-of-tetrahydroisoquinolines-and-tetrahydroisoquinolinium-salts(040536e1-22a7-47c7-bee9-51e53543ff04)"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    val fileNameHash = Http.md5HashString(actualName)
+    assert(hashName == s"$fileNameHash.74bdbe0d010151d2d42f6768eca1290c.pdf")
+  }
+
+  "A uri with filename longer than 255 chars with no query params" should "be hashed" in {
+    val origUri = Uri.parse("http://orbit.dtu.dk/en/publications/dual-nicotinic-acetylcholine-receptor-42-antagonists7-agonists-synthesis-docking-studies-and-pharmacological-evaluation-of-tetrahydroisoquinolines-and-tetrahydroisoquinolinium-salts(040536e1-22a7-47c7-bee9-51e53543ff04)-this-is-way-longer-than-it-really-should-be.pdf?nofollow=true&rendering=author")
+    val fileName = "dual-nicotinic-acetylcholine-receptor-42-antagonists7-agonists-synthesis-docking-studies-and-pharmacological-evaluation-of-tetrahydroisoquinolines-and-tetrahydroisoquinolinium-salts(040536e1-22a7-47c7-bee9-51e53543ff04)-this-is-way-longer-than-it-really-should-be.pdf"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    val fileNameHash = Http.md5HashString(fileName.replace(".pdf", ""))
+    assert(hashName == s"$fileNameHash.pdf")
+  }
+  "A url that has no path" should "not be changed" in {
+    val origUri = Uri.parse("www.bbc.co.uk")
+    val fileName = ""
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
+  "A url with a filename that is shorter than 255 chars and has a path" should "not be changed" in {
+    val origUri = Uri.parse("www.bbc.co.uk")
+    val fileName = ""
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
+  "A url filename that is shorter than 255 chars and has path" should "not be changed" in {
+    val origUri = Uri.parse("www.bbc.co.uk/news")
+    val fileName = "news"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
+  "A url filename that is shorter than 255 chars and has a path and query params" should "not be changed" in {
+    val origUri = Uri.parse("www.bbc.co.uk/news?a=b")
+    // Note this not the actual query hash, just representative
+    val fileName = "news.74bdbe0d010151d2d42f6768eca1290c"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
+  "A url filename that is shorter than 255 chars and has a path, query params and extension" should "not be changed" in {
+    val origUri = Uri.parse("www.bbc.co.uk/news.pdf?a=b")
+    // Note this not the actual query hash, just representative
+    val fileName = "news.74bdbe0d010151d2d42f6768eca1290c.pdf"
+    val hashName = Http.hashOrOriginal(origUri, fileName)
+    assert(hashName == fileName)
+  }
+
 }
